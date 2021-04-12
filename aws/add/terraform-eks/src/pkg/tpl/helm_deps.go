ccae0507ffaf840f57fb4496640e7fec6865c74e
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-12 17:59:24 -0300

    update helm_deps
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- aws/add/terraform-eks/src/pkg/tpl/helm_deps.go
+++ aws/add/terraform-eks/src/pkg/tpl/helm_deps.go
@@ -2,16 +2,6 @@ package tpl
 
 const (
 	HelmMaintf = `
-# data "helm_repository" "stable" {
-#   name = "stable"
-#   url  = "https://charts.helm.sh/stable"
-# }
-
-# data "helm_repository" "incubator" {
-#   name = "incubator"
-#   url  = "https://charts.helm.sh/incubator"
-# }
-
 data "helm_repository" "codecentric" {
   name = "codecentric"
   url  = "https://codecentric.github.io/helm-charts"
